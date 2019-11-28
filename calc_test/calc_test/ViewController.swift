40242fd09aa3b8c5066b3e18b2a83d9495143993
あきどん <akidon0000@gmail.com>
2019-11-28 14:18:39 +0900

    細かいところを修正、まだ（）に未対応。

--- calc_test/calc_test/ViewController.swift
+++ calc_test/calc_test/ViewController.swift
@@ -22,18 +22,22 @@
 // 数式 ⇒ 逆ポーランド記法(Reverse Polish Notation) ⇒ 答え
 
 import UIKit
-
 class ViewController: UIViewController {
-    var formulas:[String] = [] //計算式　  例えとして　"1+2-3*4/5" を使用　       formulas = ["1","+","2","-","3","*","4","/","5"]
-    var stacks:[String] = [] //逆ポーランドへ変換する際に一時的に四則記号を保持する    stacks = ["+","-","*","/"]  ##最終的には空のリストとなる
-    var buffa:[String] = [] //逆ポーランドへ変換した式を保持　[]                   buffa = ["1","2","3","4","*","5","/","-","+"]
-    var num:String = "" //formulas[?]を保持
-    var formulas_num:String = "" //formulasへ追加する前に数字を組み合わせるための変数　例えば formulas_num = "1" + "0" →　formulas_num = "10"
+    var formulas:[String] = [] //計算式　  例えとして　"1+2-3*4/5" を使用
+                             //formulas = ["1","+","2","-","3","*","4","/","5"]
+    var stacks:[String] = [] //逆ポーランドへ変換する際に一時的に四則記号を保持する
+                             //stacks = ["+","-","*","/"]  ##最終的には空のリストとなる
+    var buffa:[String] = [] //逆ポーランドへ変換した式を保持　[]
+                              //buffa = ["1","2","3","4","*","5","/","-","+"]
+    var num:String = ""     //formulas[?]を保持
+    var formulas_num:String = "" //formulasへ追加する前に数字を組み合わせるための変数
+                                  //例えば formulas_num = "1" + "0" →　formulas_num = "10"
     var only_formulas:String = "" //式を画面に（Fotmulas.text）に計算式として表示　それ以外の機能は持たない
     
     var nums:Int = 0
     var signal_TF = true
-    //numが数値かどうかを判断する　これはnumが　num = "1" や num = "+"を保持している可能性があるため　*引用元　https://teratail.com/questions/54252
+    //numが数値かどうかを判断する　これはnumが　num = "1" や "(",
+    //"+"を保持している可能性があるため　*引用元　https://teratail.com/questions/54252
     func isOnlyNumber(_ str:String) -> Bool {
         let predicate = NSPredicate(format: "SELF MATCHES '\\\\d+'")
         return predicate.evaluate(with: str)
@@ -44,61 +48,70 @@ class ViewController: UIViewController {
         for i in 0..<formulas.count{
             num = formulas[i]
             var Recu = true
-            if isOnlyNumber(num) {      // numが数字ならばbuffaに追加する
+            
+            if isOnlyNumber(num) {              // numが数字ならばbuffaに追加する
                 buffa.append(num)
-            } else {
-                if num == ")"{          // numが　”)” かどうかを判断
-                    //for i in Int(stacks.index(of: "(")!)...stacks.count{
-                    //  buffa.append(stacks[i])
-                        
+            }else if num == ")"{                // numが　”)” かどうかを判断
+                for _ in 0 ..< stacks.firstIndex(of: "(")!{
                     
                     
+                    print("a")
+                    print(stacks)
+                    print(buffa)
+                    print("b")
                     
-                }else{
-                    if num == "("{      // numが　"(" ならばstacksに追加
-                        stacks.append(num)
+                    
+                    if stacks[0] == "(" || stacks[0] == ""{
+                        stacks.removeFirst()
                     }else{
-                                        //stacksが空になるまでループする。　Recu == true はstacksの中に要素が存在することを指している。
-                        while Recu == true{
-                            if stacks.isEmpty{
-                                Recu = false
-                            }else{      //四則演算の優先順位を検査する。　"/" > "*" > "+" = "-"   ()は導入予定
-                                if num == "+" || num == "-"{
-                                    //numが"+"か"-"の時、stacksの一番上にある記号をbuffaに追加する。
-                                    buffa.append(stacks[0])
-                                    stacks.removeFirst()
-                                    //}else{
-                                      //  Recu = false
-                                        //何もしない　ループの最後にstacksに追加するコードをかいた。
-                                    //}
-                                    
-                                }else if num == "*"{
-                                    if stacks[0] == "/"{
-                                        buffa.append(stacks[0])
-                                        stacks.removeFirst()
-                                    }else{
-                                        Recu = false
-                                        //何もしない
-                                    }
-                                }else{   //numが"/"の時ここを通る。
-                                    Recu = false
-                                    //何もしない
-                                }
-                            }
+                            buffa.append(stacks[0])
+                            stacks.removeFirst() //buffaに追加した四則演算記号をstacksから消去
                         }
-                        // stacksに追加する
-                        stacks.insert(num, at: 0)
-                        
                     }
+            }else if num == "("{
+                stacks.insert("(", at:0)
+            }else{
+                while Recu == true{      //stacksが空になるまでループする。
+                                         //Recu == true はstacksの中に要素が存在することを指している。
+                    if stacks.isEmpty{
+                        Recu = false
+                    }else{               //四則演算の優先順位を検査する。　"/" > "*" > "+" = "-"
+                        if num == "+" || num == "-"{    //numが"+"か"-"の時、stacksの一番上にある記号をbuffaに追加する。
+                                    
+                            if stacks[0] == "(" || stacks[0] == ""{
+                                Recu = false
+                            }else{
+                                buffa.append(stacks[0])
+                                stacks.removeFirst()
+                            }
+                        }else if num == "*"{
+                            if stacks[0] == "/"{
+                                buffa.append(stacks[0])
+                                stacks.removeFirst()
+                            }else{
+                                Recu = false
+                            }
+                        }else{               //numが"/"の時ここを通る。
+                            Recu = false
+                        }
+                    }   // stacksに追加する
+                    stacks.insert(num, at: 0)  //積み木のように上からstacksに入れていき、上から取り出していく
                 }
             }
+        
+                    
         }
         for i in 0 ..< stacks.count{
-            buffa.append(stacks[i])
+                buffa.append(stacks[i])
         }
     }
+    
+            
     //逆ポーランド記法(Reverse Polish Notation) ⇒ 答え へ変換
     func Polish_To_Answer(){
+        print("c")
+        print(buffa)
+        print("d")
         stacks.removeAll()
         for i in 0 ..< buffa.count{
             if buffa[i] == "+" || buffa[i] == "-" || buffa[i] == "*" || buffa[i] == "/"{
@@ -154,7 +167,7 @@ class ViewController: UIViewController {
         signal_TF = true
     }
     
-    @IBAction func Signal(_ sender: UIButton) {  // "+" = tag 100 , "-" = tag 101 , "*" = tag 102 , "/" = tag 103
+    @IBAction func Signal(_ sender: UIButton) {  // "+" = tag 100 , "-" = tag 101 , "*" = tag 102 , "/" = tag 103 , "(" = tag 104 , ")" = tag 105
         if signal_TF == true{ //記号の重複に対応　　例えば　10++2+*-7 =  str = "abcd" str[0:1] = a
             signal_TF = false
             formulas.append(formulas_num) //数値を追加
@@ -164,11 +177,9 @@ class ViewController: UIViewController {
             
             
             
-            //
-            //*--------------ここにonly_formulas（式だけを画面に表示する変数）の後ろの文字を消して新しい記号を入れる
-            // 例えば 1+2+3+ → 1+3+3 → 1+2+3-  プラスからマイナスに変更する
-            //
-            
+//*--------------ここにonly_formulas（式だけを画面に表示する変数）の後ろの文字を消して新しい記号を入れる
+// 例えば 1+2+3+ → 1+3+3 → 1+2+3-  プラスからマイナスに変更する
+//
             
         }
         
@@ -189,11 +200,28 @@ class ViewController: UIViewController {
             only_formulas = "ERROR"
             print("error")
         }
-        print(only_formulas)
+        //print(only_formulas)
         Formula.text = only_formulas
             
     }
-            
+           
+    @IBAction func parentheses(_ sender: UIButton) {
+        switch String(sender.tag){
+            case "104":
+                only_formulas += "("
+                formulas.append("(")
+            case "105":
+                only_formulas += ")"
+                formulas.append(")")
+            default:
+                only_formulas = "ERROR"
+                print("error")
+        }
+        Formula.text = only_formulas
+    }
+    
+    
+    
     @IBAction func Equal(_ sender: Any) {
         formulas.append(formulas_num)
         formulas_num = ""
@@ -215,6 +243,7 @@ class ViewController: UIViewController {
         only_formulas = ""
         Formula.text = ""
         Ans.text = "0"
+        signal_TF = true
     }
 
     @IBOutlet weak var Formula: UILabel!
